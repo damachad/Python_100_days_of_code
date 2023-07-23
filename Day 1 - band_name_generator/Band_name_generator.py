44e074974681d1a07df42085b8976f80f50b17f9
David Correia <davidmachado98@hotmail.com>
2023-07-23 16:32:07 +0100

    Update Band_name_generator.py

--- Day 1 - band_name_generator/Band_name_generator.py
+++ Day 1 - band_name_generator/Band_name_generator.py
@@ -1,5 +1,3 @@
-# This program gives a possible Band Name by combining the name of a city with a name of a pet (inputed by the user)
-
 print("Welcome to the Band Name Generator.")
 city_name = input("What's the name of the city you grew up in?\n")
 pet_name = input("Insert a name of a pet you owned\n")
