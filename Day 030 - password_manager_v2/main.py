827bd8151fbbd47e797d5c146dab4fd4af0a8432
David Correia <davidmachado98@hotmail.com>
2023-09-07 15:14:12 +0100

    Update main.py

--- Day 030 - password_manager_v2/main.py
+++ Day 030 - password_manager_v2/main.py
@@ -90,7 +90,7 @@ window.title("Password Manager")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
-logo = PhotoImage(file="../logo.png")
+logo = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo)
 canvas.grid(row=0, column=1)
 
